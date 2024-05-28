e2e0a6154016567625cbaa8ee04eca1d55e8eff0
FaizanAnsari185 <superseupermfa@gmail.com>
2024-05-28 17:08:05 +0530

    scroll id and gradient add

--- components/ScrollUp.jsx
+++ components/ScrollUp.jsx
@@ -4,7 +4,7 @@ import React from 'react'
 
 const ScrollUp = () => {
   return (
-    <Link href={"#home"} className="fixed z-50 bottom-5 right-5 h-12 w-12 p-3 bg-purple-600 rounded-full border-2 hover:bg-black hover:text-purple-600 hover:border-2 border-purple-600 transition ease-in-out duration-300">
+    <Link href={"#home"} className="fixed z-50 bottom-5 right-5 h-12 w-12 p-3 bg-gradient-to-tl from-purple-600 to-black hover:from-black hover:to-black rounded-full border-2 hover:text-purple-600 hover:border-2 border-purple-600 transition ease-in-out duration-300">
         <Image className="h-full w-full" src="/assets/UpLogo.svg" alt="" height={100} width={100} />    
       </Link>
   )
